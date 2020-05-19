069c9d5cde67e872eb5ea8267a1ddf9d77bdfe4c
wwheeler <wheeler.william@epa.gov>
2020-05-19 19:01:42 +0000

    add num_words to graphs

--- graphs.R
+++ graphs.R
@@ -1,7 +1,6 @@
-library(purrr)
-library(ggplot2)
-library(ggthemes)
+library(tidyverse)
 library(reshape2)
+
 
 years <- as.character(seq(1998, 2018))
 
@@ -27,11 +26,13 @@ proc.time() - ptm
 
 all_sections_incorporation <- all_sections_processed %>%
   mutate(incorporation = str_detect(section_subject, "Incorporation")*1,
-         CFR_cite = str_count(paragraph_text, "CFR")*1) %>%
+         CFR_cite = str_count(paragraph_text, "CFR")*1,
+         num_words = str_count(paragraph_text, boundary(type = "word"))) %>%
   group_by(CFR_year) %>%
   summarize(all_rows = n()/100 , 
             all_incorporations = sum(incorporation, na.rm = TRUE),
-            all_CFRs = sum(CFR_cite, na.rm = TRUE)) %>%
+            all_CFRs = sum(CFR_cite, na.rm = TRUE),
+            all_words_millions = sum(num_words, na.rm = TRUE)/1000000) %>%
   ungroup() 
 
 
@@ -47,7 +48,7 @@ ggplot(data = subset(all_sections_graph, variable == "all_incorporations"),
   labs( y = "Count", x = "CFR year", title = "Growth in Incorporations by Reference, EPA CFR") +
   theme_minimal()
 
-ggsave("incorp by reference.png")
+#ggsave("./graphs/incorp by reference.png")
 
 ggplot(data = subset(all_sections_graph, variable == "all_CFRs"),
        aes(x = CFR_year, y = value)) +
@@ -56,4 +57,12 @@ ggplot(data = subset(all_sections_graph, variable == "all_CFRs"),
   labs( y = "Count", x = "CFR year", title = "Growth in CFR References, EPA CFR") +
   theme_minimal()
 
-ggsave("use of CFR.png")
+#ggsave("use of CFR.png")
+
+
+ggplot(data = subset(all_sections_graph, variable == "all_words_millions"),
+       aes(x = CFR_year, y = value)) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(2002, 2018, by =2)) +
+  labs( y = "Count", x = "CFR year", title = "Growth in Number of Words (in Millions), EPA CFR") +
+  theme_minimal()
